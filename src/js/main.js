ba601b18135bc94aaabdef396af222ea44cb1d24
ArrrtSam <38813128+ArrrtSam@users.noreply.github.com>
2019-07-09 17:59:33 +0200

    add turbine-repair

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 var breakpoint = false;
-
 $(window).scroll(function() {
   if ($(this).scrollTop() > 80 && !breakpoint ) {
      addFix();
@@ -8,7 +7,6 @@ $(window).scroll(function() {
      removeFix();
   }
 })
-
 function addFix() {
   breakpoint = true;
   $('header').addClass('fixed')
@@ -34,57 +32,9 @@ $('#menu-shadow').click(function(){
 $('.menu_toggle').click(function(e){
   e.preventDefault();
   $(this).parent().siblings(".dropdown").slideToggle();
-})
-$('.exploitation-carousel').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  speed: 1500
-});
-$('.clients-carousel').slick({
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  speed: 1500,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
 });
 $('.captcha-btn').click(function(e){
   e.preventDefault();
-});
-$('#phone').mask('+38(000) 000-00-00');
-
-$('.our_works_album').lightGallery();
-
-$('.reviews_carousel').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  speed: 1500
 });
 $('.map-img').click(function(e){
   e.preventDefault();
