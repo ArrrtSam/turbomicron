f5e972dbd5f371d08ef9125863e6750474ac3dc2
ArrrtSam <38813128+ArrrtSam@users.noreply.github.com>
2019-07-15 23:17:29 +0200

    add turbines-product

--- src/js/main.js
+++ src/js/main.js
@@ -143,3 +143,31 @@ $('.reviews_carousel').slick({
   autoplaySpeed: 2000,
   speed: 1500
 });
+$('.relation-carousel').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 1500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
